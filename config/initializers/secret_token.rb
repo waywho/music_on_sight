8387eec26607bc182124572f72ea73d8f102de1f
WeiHsi Hu <weihsi.hu@gmail.com>
2015-01-19 22:32:01 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicOnSight::Application.config.secret_key_base = 'cf4abd6d1f3efb77844e4fd9e1e8eabcf44d39495ba65a72a28aa51a02c92016dfdeaf03237550c8ebff400bc62da869bec413ce0d216ffb6d019897adf0e6da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MusicOnSight::Application.config.session_store :cookie_store, key: '_music_on_sight_session'
