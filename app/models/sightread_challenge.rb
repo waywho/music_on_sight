1a01c120229f606d918ade322a41c5fabcae182c
WeiHsi Hu <weihsi.hu@gmail.com>
2015-01-29 03:00:23 +0000

    add note check method

--- app/models/sightread_challenge.rb
+++ app/models/sightread_challenge.rb
@@ -1,3 +1,18 @@
 class SightreadChallenge < ActiveRecord::Base
 	has_many :notes
+
+	# def eval_notes
+	# 	sound = AubioWraper.new
+	# 	sound.parsed_aubionotes
+
+	# 	sound.each do |midi, on, off|
+	# 		on_before = sound.at(sound.index(on) - 1)
+	# 		have_note != on.nil
+
+	# 		if have_note
+	# 			puts "Well done!" if midi/@sightreading.note > .80
+	# 			puts "Good timing!" if (on - on_before) /@sightreading.duration > .90
+	# 		end
+	# 	end
+	# end
 end
