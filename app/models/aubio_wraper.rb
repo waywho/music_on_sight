2acfa1f5676e74bf5e0e57b0c70e1385d6d19715
WeiHsi Hu <weihsi.hu@gmail.com>
2015-01-29 00:44:24 +0000

    Adding vextab js and css

--- app/models/aubio_wraper.rb
+++ app/models/aubio_wraper.rb
@@ -2,11 +2,11 @@ class AubioWraper
 	attr_accessor :wave_file
 
 	def initialize
-		@wave_file = Rails.root.join('data', 'test.wav').to_s
+		@wave_file = Rails.root.join('data', 'test2.wav').to_s
 	end
 
 	def aubionotes_command
-		"aubionotes -i #{self.wave_file}"
+		"aubionotes -i #{self.wave_file} -t 1.3 -s -70" 
 	end
 
 	def exec_aubionotes_command
