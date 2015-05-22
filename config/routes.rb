54028a80a5793c3488d9786aaa399712e3a66ea9
WeiHsi <weihsi.hu@gmail.com>
2015-05-22 21:56:24 +0000

    add admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MusicOnSight::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
   resource :dashboard, :only => [:show]
   root 'static_pages#index'
